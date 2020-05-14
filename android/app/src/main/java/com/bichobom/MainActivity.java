e9641ec3a492d5b66b1bb2489757515c1bd16cec
Daniel Pereira <daniel.prr.azevedo@gmail.com>
2020-05-14 01:59:23 -0300

    Revert "up"
    This reverts commit 5f1781b4b266d1f9b0daae04a50cc134fc7182ca.

--- android/app/src/main/java/com/bichobom/MainActivity.java
+++ android/app/src/main/java/com/bichobom/MainActivity.java
@@ -2,6 +2,7 @@ package com.bichobom;
 
 import android.os.Bundle;
 
+import com.baumblatt.capacitor.firebase.auth.CapacitorFirebaseAuth;
 import com.getcapacitor.BridgeActivity;
 import com.getcapacitor.Plugin;
 
@@ -16,6 +17,7 @@ public class MainActivity extends BridgeActivity {
     this.init(savedInstanceState, new ArrayList<Class<? extends Plugin>>() {{
       // Additional plugins you've installed go here
       // Ex: add(TotallyAwesomePlugin.class);
+      add(CapacitorFirebaseAuth.class);
     }});
   }
 }
